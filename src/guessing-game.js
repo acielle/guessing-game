e6f2ed51075e83ccea5f53fee14e1b2583bdac8b
acielle <sg23mj@gmail.com>
2022-08-17 00:49:04 +0300

    init: start task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        this.midNumber = Math.round((this.minNumber + this.maxNumber)/2);
+        return this.midNumber;
     }
 
     lower() {
-
+        this.maxNumber = this.midNumber--;
     }
 
     greater() {
-
+        this.minNumber = this.midNumber++;
     }
 }
 
